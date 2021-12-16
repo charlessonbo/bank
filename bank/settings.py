45b3e57224ebc20dec04424904d86a2e964af937
charlessonbo <66862592+charlessonbo@users.noreply.github.com>
2021-12-16 23:31:55 +0800

    add new files heroku

--- bank/settings.py
+++ bank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@pw(ub@0xb)oqqi2!8z_xh2!yrb+k+#598g(yrg#lrcxd6&5(j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['charlessonbankatm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
